ca75aa108572bf759ac8249c7d31bfea73ca38f0
Sujith1219 <68234378+SujithGunasekaran@users.noreply.github.com>
2021-01-16 20:38:05 +0530

    Firebase config file updated key removed

--- client/src/Firebase/config.js
+++ client/src/Firebase/config.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCycK_XKx90kg8EEEVaXHcbJsFtMGgBjtk",
-    authDomain: "image-gallery-973de.firebaseapp.com",
-    projectId: "image-gallery-973de",
-    storageBucket: "image-gallery-973de.appspot.com",
-    messagingSenderId: "862862841325",
-    appId: "1:862862841325:web:b46ab6347417603db543e4"
-};
+var firebaseConfig = { '<<<<<<<<< Add your Firebase config >>>>>>>>>>' };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
