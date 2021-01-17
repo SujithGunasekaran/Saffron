6fe5d6fc7aa0f888840c978d511fb5b3f0e50348
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-17 21:16:39 +0530

    Route added for login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/Login" exact component={Login} />
+        <Route path="/Signup" exact component={Signup} />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
