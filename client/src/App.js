c4208684327ad56d1f74a810d1b1658fe28f21e4
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-18 19:29:51 +0530

    All pages connected to store

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
+import store from './store';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/Login" exact component={Login} />
-        <Route path="/Signup" exact component={Signup} />
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/Login" exact component={Login} />
+          <Route path="/Signup" exact component={Signup} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
