38e70c6b23263ab513a8070b2e84c65745e534ed
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-18 19:28:46 +0530

    Store.js added

---
+++ client/src/store.js
@@ -0,0 +1,39 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import AllReducers from './Redux/reducer';
+
+const saveToLocalStorage = (state) => {
+    try {
+        let stringData = JSON.stringify(state);
+        localStorage.setItem('state', stringData)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        let stringData = localStorage.getItem('state');
+        if (stringData === null) return undefined;
+        return JSON.parse(stringData)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+let storedReduxData = loadFromLocalStorage();
+
+const store = createStore(
+    AllReducers,
+    storedReduxData,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
+
+export default store
