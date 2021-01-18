8e2296a4557a96cbe4ff9a96d6b68ecb6802d7d5
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-18 19:26:31 +0530

    Login and Signup form connect to redux store

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import '../css/form.css';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
+import { setUserName, setUserToken } from '../Redux/actions/AthenticateAction';
+import { connect } from 'react-redux';
 
-function Login() {
+function Login(props) {
 
     const [userInfo, setUserInfo] = useState({ username: '', password: '' })
     const [fieldNameError, setFieldNameError] = useState({ usernameError: '', passwordError: '' })
@@ -35,7 +37,8 @@ function Login() {
             const userData = { username: username, password: password };
             try {
                 let responseData = await axios.post('http://localhost:5000/userData/user/login', userData)
-                console.log(responseData.data)
+                props.setUserName(responseData.data)
+                props.setUserToken('asdas!@#!@jsbdfjbs.12(ssfsdf')
             }
             catch (err) {
                 setLoginError('Invalid Username or Password')
@@ -58,60 +61,78 @@ function Login() {
         return result;
     }
 
-    return (
-        <div>
-            <div className="form-main">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-4 mx-auto">
-                            <div className="form-container">
-                                <div className="form-logo-name">Saffron</div>
-                                <div className="form-heading-name">Welcome Back, To Gallery Store</div>
-                                <hr className="form-heading-name-hr" />
-                                <form onSubmit={handleFormSubmit}>
-                                    <label className="form-label">Username :</label>
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        className="form-input-field"
-                                        value={userInfo.username}
-                                        onChange={handleInputChange}
-                                    />
-                                    {
-                                        fieldNameError['usernameError'] ? <div className="form-error">{fieldNameError['usernameError']}</div> : null
-                                    }
-                                    <label className="form-label">Password :</label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        className="form-input-field"
-                                        value={userInfo.password}
-                                        onChange={handleInputChange}
-                                    />
-                                    {
-                                        fieldNameError['passwordError'] ? <div className="form-error">{fieldNameError['passwordError']}</div> : null
-                                    }
-                                    <button className="form-submit-btn">Sign in</button>
-                                    {
-                                        loginError ? <div className="form-error">{loginError}</div> : null
-                                    }
-                                    <div className="form-having-account">Don't have an Account? <Link to="/signup">Signup</Link></div>
-                                </form>
+    if (props.AuthenticateReducer.usertoken) {
+        return <Redirect to='/' />
+    }
+    else {
+        return (
+            <div>
+                <div className="form-main">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-4 mx-auto">
+                                <div className="form-container">
+                                    <div className="form-logo-name">Saffron</div>
+                                    <div className="form-heading-name">Welcome Back, To Gallery Store</div>
+                                    <hr className="form-heading-name-hr" />
+                                    <form onSubmit={handleFormSubmit}>
+                                        <label className="form-label">Username :</label>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            className="form-input-field"
+                                            value={userInfo.username}
+                                            onChange={handleInputChange}
+                                        />
+                                        {
+                                            fieldNameError['usernameError'] ? <div className="form-error">{fieldNameError['usernameError']}</div> : null
+                                        }
+                                        <label className="form-label">Password :</label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            className="form-input-field"
+                                            value={userInfo.password}
+                                            onChange={handleInputChange}
+                                        />
+                                        {
+                                            fieldNameError['passwordError'] ? <div className="form-error">{fieldNameError['passwordError']}</div> : null
+                                        }
+                                        <button className="form-submit-btn">Sign in</button>
+                                        {
+                                            loginError ? <div className="form-error">{loginError}</div> : null
+                                        }
+                                        <div className="form-having-account">Don't have an Account? <Link to="/signup">Signup</Link></div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <hr className="form-footer-hr" />
-                            <div className="form-footer">
-                                <div className="form-footer-name"><Link to="/">Home</Link></div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <hr className="form-footer-hr" />
+                                <div className="form-footer">
+                                    <div className="form-footer-name"><Link to="/">Home</Link></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Login
+const mapStateToProps = (state) => (
+    {
+        AuthenticateReducer: state.AuthenticateReducer
+    }
+)
+
+const mapDispatchToProps = (dispatch) => (
+    {
+        setUserName: (username) => dispatch(setUserName(username)),
+        setUserToken: (usertoken) => dispatch(setUserToken(usertoken))
+    }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 function Signup() {
@@ -73,63 +73,68 @@ function Signup() {
         return result;
     }
 
-    return (
-        <div>
-            <div className="form-main">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-4 mx-auto">
-                            <div className="form-container">
-                                <div className="form-logo-name">Saffron</div>
-                                <div className="form-heading-name">Welcome To Gallery Store</div>
-                                <hr className="form-heading-name-hr" />
-                                <form onSubmit={handleFormSubmit}>
-                                    <label className="form-label">Username :</label>
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        className="form-input-field"
-                                        value={userInfo.username}
-                                        onChange={handleInputChange}
-                                    />
-                                    {
-                                        fieldNameError['usernameError'] ? <div className="form-error">{fieldNameError['usernameError']}</div> : null
-                                    }
-                                    <label className="form-label">Password :</label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        className="form-input-field"
-                                        value={userInfo.password}
-                                        onChange={handleInputChange}
-                                    />
-                                    {
-                                        fieldNameError['passwordError'] ? <div className="form-error">{fieldNameError['passwordError']}</div> : null
-                                    }
-                                    <button className="form-submit-btn">Create Account</button>
-                                    {
-                                        signupError ? <div className="form-error">{signupError}</div> : null
-                                    }
-                                    {
-                                        signupSuccess ? <div className="form-signup-success">{signupSuccess} <Link to="/Login">Click here</Link> to Sign-in</div> : null
-                                    }
-                                    <div className="form-having-account">Already have an Account? <Link to="/Login">Signin</Link></div>
-                                </form>
+    if (localStorage.getItem('token')) {
+        return <Redirect to='/' />
+    }
+    else {
+        return (
+            <div>
+                <div className="form-main">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-4 mx-auto">
+                                <div className="form-container">
+                                    <div className="form-logo-name">Saffron</div>
+                                    <div className="form-heading-name">Welcome To Gallery Store</div>
+                                    <hr className="form-heading-name-hr" />
+                                    <form onSubmit={handleFormSubmit}>
+                                        <label className="form-label">Username :</label>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            className="form-input-field"
+                                            value={userInfo.username}
+                                            onChange={handleInputChange}
+                                        />
+                                        {
+                                            fieldNameError['usernameError'] ? <div className="form-error">{fieldNameError['usernameError']}</div> : null
+                                        }
+                                        <label className="form-label">Password :</label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            className="form-input-field"
+                                            value={userInfo.password}
+                                            onChange={handleInputChange}
+                                        />
+                                        {
+                                            fieldNameError['passwordError'] ? <div className="form-error">{fieldNameError['passwordError']}</div> : null
+                                        }
+                                        <button className="form-submit-btn">Create Account</button>
+                                        {
+                                            signupError ? <div className="form-error">{signupError}</div> : null
+                                        }
+                                        {
+                                            signupSuccess ? <div className="form-signup-success">{signupSuccess} <Link to="/Login">Click here</Link> to Sign-in</div> : null
+                                        }
+                                        <div className="form-having-account">Already have an Account? <Link to="/Login">Signin</Link></div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <hr className="form-footer-hr" />
-                            <div className="form-footer">
-                                <div className="form-footer-name"><Link to="/">Home</Link></div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <hr className="form-footer-hr" />
+                                <div className="form-footer">
+                                    <div className="form-footer-name"><Link to="/">Home</Link></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Signup
