202c58531293f1a8bad9869d6226ea9521020e13
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-18 19:28:31 +0530

    Header.js username display from store

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import '../css/header.css';
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import { FiLogOut } from 'react-icons/fi';
+import { connect } from 'react-redux';
+import { setUserName, setUserToken } from '../Redux/actions/AthenticateAction';
 
-export default function Header() {
+
+function Header(props) {
+
+    const { username } = props.AuthenticateReducer;
+
+    const removerUsername = () => {
+        props.setUserName('')
+        props.setUserToken('')
+    }
+
     return (
         <div>
             <div className="header-container">
                 <div className="header-main">
                     <div className="header-logo-name">Saffron</div>
-                    <div className="header-login-name">
-                        <Link to='/Login'>Signin</Link> / <Link to="Signup">Signup</Link>
-                    </div>
+                    {
+                        username === '' ?
+                            <div className="header-login-name">
+                                <Link to='/Login'>Signin</Link> / <Link to="Signup">Signup</Link>
+                            </div> :
+                            <div className="header-user-name-display">
+                                <div className="header-tooltip">
+                                    <FiLogOut className="header-user-logout" onClick={() => removerUsername()} />
+                                    <span className="header-tooltip-open">Logout</span>
+                                </div>
+                                <div className="header-user-name">{username}</div>
+                                <Avatar className="header-user-avatar">{username.charAt(0)}</Avatar>
+                            </div>
+                    }
+
                 </div>
             </div>
         </div>
     )
 }
+
+const mapStateToProps = (state) => ({
+    AuthenticateReducer: state.AuthenticateReducer
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    setUserName: (username) => dispatch(setUserName(username)),
+    setUserToken: (usertoken) => dispatch(setUserToken(usertoken))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
