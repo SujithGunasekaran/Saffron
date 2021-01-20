ee1171fa9c10bb207288db501aa42c6a3f5849b2
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-20 20:27:19 +0530

    Sending username and filename reset

--- client/src/Components/Progress.js
+++ client/src/Components/Progress.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { withStyles } from "@material-ui/core/styles";
 import LinearProgress from '@material-ui/core/LinearProgress';
 import useFirestore from '../Hooks/useFirestore';
+import { connect } from 'react-redux';
 
 
-export default function Progress({ file, setFileName }) {
+function Progress(props) {
+
+    const { file, setFileName } = props;
+    const { username } = props.AuthenticateReducer;
+
     const StyledLinearProgress = withStyles({
         colorPrimary: {
             backgroundColor: "#F3E3E3"
@@ -13,7 +18,7 @@ export default function Progress({ file, setFileName }) {
             backgroundColor: "rgb(241, 153, 169)"
         }
     })(LinearProgress);
-    const { progress, imageUploadeError, URL } = useFirestore(file)
+    const { progress, imageUploadeError, URL } = useFirestore(file, username)
     if (URL) {
         setFileName('')
     }
@@ -31,3 +36,9 @@ const styles = {
         marginTop: '20px',
     }
 }
+
+const mapStateToProps = (state) => ({
+    AuthenticateReducer: state.AuthenticateReducer
+})
+
+export default connect(mapStateToProps)(Progress)
