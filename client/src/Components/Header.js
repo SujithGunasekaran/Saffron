37c9bae79299b8a4ffe355583d53f280d8d61197
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-20 20:29:54 +0530

    On logout Clear data in localStorage

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -14,6 +14,7 @@ function Header(props) {
     const removerUsername = () => {
         props.setUserName('')
         props.setUserToken('')
+        window.localStorage.clear('state')
     }
 
     return (
