04b72de96ee412f70b8d92c0ff236b186d139f34
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-24 17:56:08 +0530

    Home.js based on screen width image setImageHeight

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Components/Header';
 import ImageUpload from '../Components/ImageUpload';
 import ImageCard from '../Components/ImageCard';
 import ImageModel from '../Components/ImageModel';
+import ImageHeight from '../Json/screenSize.json';
 
 export default function Home() {
 
-    const [selectedImage, setSelectedImage] = useState('')
+    const [selectedImage, setSelectedImage] = useState('');
+    const [imageHeight, setImageHeight] = useState('');
+
+    useEffect(() => {
+        let imageHeight;
+        Object.keys(ImageHeight).forEach((size) => {
+            const { height } = ImageHeight[size];
+            if (size < window.innerWidth) imageHeight = height;
+        })
+        setImageHeight(imageHeight);
+    }, [window.innerWidth])
+
 
     return (
         <div>
             <Header />
             <ImageUpload />
-            <ImageCard setSelectedImage={setSelectedImage} />
+            <ImageCard setSelectedImage={setSelectedImage} imageHeight={imageHeight} />
             {
                 selectedImage ? <ImageModel selectedImage={selectedImage} setSelectedImage={setSelectedImage} /> : null
             }
