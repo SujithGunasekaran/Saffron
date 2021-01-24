6c2e84218ee51cf1476c0aedb2ce5a6ace36b761
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-24 17:56:45 +0530

    Image model updated

--- client/src/Components/ImageModel.js
+++ client/src/Components/ImageModel.js
@@ -4,8 +4,8 @@ import '../css/imageModel.css';
 export default function ImageModel({ selectedImage, setSelectedImage }) {
     return (
         <div>
-            <div className="image-model-main" onClick={() => setSelectedImage('')}>
-                <div className="image-model-close-btn" />
+            <div className="image-model-main" >
+                <div className="image-model-close-btn" onClick={() => setSelectedImage('')} />
                 <img src={selectedImage} alt="image" className="image-model-img-view" />
             </div>
         </div>
