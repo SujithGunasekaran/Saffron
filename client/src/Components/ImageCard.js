4fd532b31464dad5c1dbed6436de0b003bf5940f
SujithGunasekaran <mydevelopstar@gmail.com>
2021-01-24 17:57:56 +0530

    imageCard updated

--- client/src/Components/ImageCard.js
+++ client/src/Components/ImageCard.js
@@ -1,43 +1,15 @@
-import React, { useEffect, useState } from 'react';
-// import useFirebaseDb from '../Hooks/useFirebaseDb';
-import { galleryFireStore } from '../Firebase/config';
+import React from 'react';
+import useFirebaseDb from '../Hooks/useFirebaseDb';
 import { FiDownload } from 'react-icons/fi';
 import '../css/imageCard.css';
 
 
-export default function ImageCard({ setSelectedImage }) {
+export default function ImageCard({ setSelectedImage, imageHeight }) {
 
-    const [imageList, setImageList] = useState([])
-
-    useEffect(() => {
-        const db = galleryFireStore.collection('images')
-            .orderBy('createdAt', 'desc')
-            .onSnapshot((snap) => {
-                let list = [];
-                snap.forEach((image) => {
-                    list.push({
-                        ...image.data(),
-                        id: image.id
-                    })
-                })
-                setImageList(list)
-            })
-        return () => db()
-    }, [])
-
+    const { imageList } = useFirebaseDb('images');
     const downloadImage = (url, imageName) => {
-        var element = document.createElement('a');
-        element.setAttribute('href',
-            'data:text/plain;charset=utf-8, '
-            + encodeURIComponent(url));
-        element.setAttribute('download', imageName);
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
+        window.open(url)
     }
-
-    // const { imageList } = useFirebaseDb('images');
-    // console.log(imageList)
 
     return (
         <div>
@@ -47,7 +19,7 @@ export default function ImageCard({ setSelectedImage }) {
                         {
                             imageList.length > 0 && imageList.map((image, index) => (
                                 <div className="col-md-4" key={index}>
-                                    <img className="image-card-image" src={image.url} alt="image" onClick={() => setSelectedImage(image.url)} />
+                                    <img className="image-card-image" height={imageHeight} src={image.url} alt="image" onClick={() => setSelectedImage(image.url)} />
                                     <div className="image-info-display">
                                         <div className="image-card-username">Published By : {image.username}</div>
                                         <FiDownload className="image-card-download-icon" onClick={() => downloadImage(image.url, image.imageName)} />
